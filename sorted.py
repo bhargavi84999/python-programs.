31a434fea9fd60311d8df30e6ecef239260004fe
bhargavi84999 <bhargavi84999@gmail>
2026-05-28 11:03:21 +0530

    feat:upload

---
+++ sorted.py
@@ -0,0 +1,11 @@
+l=[23,21,27,28,44,46]
+a=sorted(l,key=lambda x:x%7,reverse=True)
+print(a)
+
+a=sorted(l,key=lambda x:x%7,reverse=False)
+print(a)
+
+l=[21,3,2,5,22,6,32]
+b=sorted(l,key=lambda x:x%3)
+print(b)
+
